6250351d4b9ad6db5f509a8c7a6a78f8c78a75aa
unknown <askhat1008@gmail.com>
2022-06-08 14:08:36 -0700

    frontend fix

--- Frontend/src/routes/auth/__layout@root.svelte
+++ Frontend/src/routes/auth/__layout@root.svelte
@@ -2,11 +2,10 @@
 
 <script context="module" lang="ts">
     // import {get} from 'svelte/store';
-    import {isAuthenticated} from "$lib/stores/store";
+    import {isAuthenticated} from "$lib/stores/auth";
     import type { LoadOutput, LoadInput } from '@sveltejs/kit/types.internal';
     import { writable,get } from 'svelte/store';
 
-    // get(isAuthenticated)
     export async function load(): Promise<LoadOutput> {
         if(get(isAuthenticated)) return { status: 302, redirect: '/overview' }
         else return {}
