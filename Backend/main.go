734368f9e0a33a173eeca170c8f2a45157c2a581
unknown <askhat1008@gmail.com>
2022-06-12 13:13:38 -0700

    fix connections

--- Backend/main.go
+++ Backend/main.go
@@ -1,26 +1,31 @@
 package main
 
 import (
+	"flag"
 	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"limbic/controllers/auth"
+	"limbic/controllers/emotions"
 	"limbic/controllers/payment"
 	"limbic/controllers/users"
 	"limbic/models"
+	"log"
 )
 
 func main() {
-	dsn := "host=localhost user=root password=CSSE1810da dbname=limbic port=5432"
+	dsn := "host=db user=root password=CSSE1810da dbname=limbic port=5432"
 	db := models.Init(dsn)
 
-	//serverAddr := flag.String("addr", "localhost:50052", "The server address in the format of host:port")
-	//conn, err := grpc.Dial(*serverAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	//if err != nil {
-	//	log.Fatalf("fail to dial: %v", err)
-	//}
-	//defer conn.Close()
+	serverAddr := flag.String("addr", "service:50052", "The server address in the format of host:port")
+	conn, err := grpc.Dial(*serverAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+	defer conn.Close()
 
 	r := gin.Default()
-	//emotions.RegisterRoutes(r, conn)
+	emotions.RegisterRoutes(r, conn)
 	users.RegisterRoutes(r, db)
 	auth.RegisterRoutes(r, db)
 	payment.RegisterRoutes(r, db)
